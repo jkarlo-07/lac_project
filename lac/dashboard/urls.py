b5c7557bd07ab1e3d381b9a8708be2b405713d48
jkarlo-07 <karloachico07@gmail.com>
2024-08-16 15:58:23 +0800

    dashboard

--- lac/dashboard/urls.py
+++ lac/dashboard/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'dashboard'
 urlpatterns = [
     path('', views.dashboard_view, name='dashboard'),
+    path('guest', views.guest_view, name='guest'),
+    path('booking', views.booking_view, name='booking'),
+    path('room', views.room_view, name='room'),
 ]
--- lac/dashboard/views.py
+++ lac/dashboard/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 def dashboard_view(request):
     return render(request, "dashboard/dashboard.html")
 
+def guest_view(request):
+    return render(request, "dashboard/guest.html")
+
+def room_view(request):
+    return render(request, "dashboard/room.html")
+
+def booking_view(request):
+    return render(request, "dashboard/booking.html")
