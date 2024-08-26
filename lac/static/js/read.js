0e0ca43f1a52459e8c88a5390b94ed0445b2ebb2
jkarlo-07 <karloachico07@gmail.com>
2024-08-26 13:44:24 +0800

    step 2 booking

---
+++ lac/static/js/read.js
@@ -0,0 +1,23 @@
+console.log("hello");
+
+const roomDiv = document.querySelector(".room-expand-small");
+const readButton = document.querySelector(".expand-button-hidden");
+const secondParagraph = roomDiv.querySelectorAll('p')[1];
+secondParagraph.style.display = "none";
+
+console.log(readButton.textContent);
+
+readButton.addEventListener("click", function () {
+    event.preventDefault();
+    if (readButton.textContent === "Read Less") {
+        secondParagraph.style.display = "none";
+        readButton.textContent = "Read More";
+    } else {
+        secondParagraph.style.display = "block";
+        readButton.textContent = "Read Less";
+    }
+})
+
+
+
+
