b68ca56ffe25dc788ef09cf68bd72ba9442c135d
russel-122 <tulipat9@gmail.com>
2024-09-01 10:12:57 +0800

    add signup

--- lac/users/views.py
+++ lac/users/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 def login_view(request):
     return render(request, "users/login.html")
 
 def signup_view(request):
-    return render(request, "users/signup.html")
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  
+            return redirect('users:login') 
+    else:
+        form = UserCreationForm()
+    
+    return render(request, "users/signup.html", {"form": form})
