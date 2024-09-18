9704c3454b323d8637a4dffe9bdbd917eca20b66
jkarlo-07 <karloachico07@gmail.com>
2024-09-18 11:35:00 +0800

    make the users auth customize

--- users/models.py
+++ users/models.py
@@ -1,5 +1,37 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from django.utils import timezone
 
-class CustomUser(AbstractUser):
-    email_confirm_code = models.CharField(max_length=4, blank=True, null=True)
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128)  
+    last_login = models.DateTimeField(null=True, blank=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    email_confirm_code = models.CharField(max_length=4, blank=True, null=True) 
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']  # Include any additional required fields
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
