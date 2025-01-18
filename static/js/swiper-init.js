565df212a9dc6f5aaba2a7d434fd7e4d9a743ee7
Jerickivan29 <camalig.j29@gmail.com>
2025-01-18 20:10:42 +0800

    rtype filtering design

---
+++ static/js/swiper-init.js
@@ -0,0 +1,17 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  loop: true,
+});
